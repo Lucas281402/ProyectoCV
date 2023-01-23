f4865adacbb87ae4ef2a0407ce78cfef968948b6
Lucas Emanuel Faur <lucas_river_2814@hotmail.com>
2023-01-23 17:39:06 -0300

    modal, funciones y fuentes

--- funciones.js
+++ funciones.js
@@ -1,5 +1,11 @@
 let menu = document.getElementById('mostrar');
 
+let fuente = document.querySelector('body');
+
+let abrirModal = document.getElementById('carta_presentacion');
+let modal = document.querySelector('.modal');
+let cerrarModal = document.querySelector('.cerrar_modal');
+
 function mostrar_navbar() {
     menu.style.display =  'block';
 };
@@ -7,3 +13,16 @@ function mostrar_navbar() {
 function ocultar_navbar() {
     menu.style.display = 'none';
 };
+
+function cambiar_fuente() {
+    fuente.style.fontFamily = 'Nova Square', cursive;
+};
+
+abrirModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add('mostrar--modal');
+});
+
+cerrarModal.addEventListener('click', () => {
+    modal.classList.remove('mostrar--modal');
+});
