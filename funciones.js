ca444f722046ddcbe7ed7d2257a5a8470f53e106
Lucas Emanuel Faur <lucas_river_2814@hotmail.com>
2023-01-23 17:21:36 -0300

    inicio del header

---
+++ funciones.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById('mostrar');
+
+function mostrar_navbar() {
+    menu.style.display =  'block';
+};
+
+function ocultar_navbar() {
+    menu.style.display = 'none';
+};
